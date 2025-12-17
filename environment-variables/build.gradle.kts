99e3194972b98c67069ef5b9a52e1dd5a9ca0427
contour-gara <65527071+contour-gara@users.noreply.github.com>
2025-12-17 21:43:01 +0900

    first commit

---
+++ environment-variables/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(libs.junit.jupiter)
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation(libs.guava)
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+application {
+    mainClass = "org.example.AppKt"
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
